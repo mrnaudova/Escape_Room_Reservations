f02e7cc473501649a29b6bc9b080276062a34057
Мирослава Арнаудова <ma08096018@edu.mon.bg>
2023-02-13 21:51:45 +0200

    Add project files.

---
+++ Escape_Room_Reservations/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Escape_Room_Reservations.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
---
+++ Escape_Room_Reservations/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Escape_Room_Reservations.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
